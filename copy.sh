2b5ed2d46dc9340b9a864536bfe2f638d39aa390
Athmane Madjoudj <athmanem@gmail.com>
2019-05-22 02:08:23 +0200

    Sync

--- copy.sh
+++ copy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cp ~/.i3/config i3/
-cp ~/.i3status.conf i3/
+cp ~/.i3/config .i3/
+cp ~/.i3status.conf .i3/
 cp ~/.Xresources .
 cp ~/.vimrc .
 cp ~/.screenrc .
-cp ~/.tmux.conf .
+#cp ~/.tmux.conf .
 cp ~/.bashrc .
 cp ~/.bash_profile .
