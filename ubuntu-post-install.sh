6c51bf2272a2b1b5f728a303c2dcdf6851295d84
Othman Madjoudj <athmanem@gmail.com>
2020-05-14 23:10:56 +0200

    Create ubuntu-post-install.sh

---
+++ ubuntu-post-install.sh
@@ -0,0 +1,21 @@
+sudo apt install ecryptfs-utils cryptsetup vim vim-gtk3 virtualbox vagrant byobu build-essential ufw gufw gnome-tweak-tool gnome-shell-extension-caffeine clamtk clamdscan git fonts-powerline xterm putty calibre fonts-terminus xfonts-terminus
+
+sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager virtinst wireshark
+
+
+sudo apt remove nfs-kernel-server
+
+sudo snap install  keepassxc spotify vlc
+
+for i in vscode zaproxy skype ; do
+  sudo snap install --classic $i
+done
+
+
+
+## Vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+    
