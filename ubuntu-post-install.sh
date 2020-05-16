5e606085a950325369ffff588971ed3c8ca9c417
Othman Madjoudj <athmanem@gmail.com>
2020-05-17 01:55:25 +0200

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -9,7 +9,7 @@ sudo apt -y  install ecryptfs-utils cryptsetup vim vim-gtk3 virtualbox vagrant b
 
 sudo snap install  keepassxc spotify vlc
 
-for i in code zaproxy skype ; do
+for i in code skype ; do
   sudo snap install --classic $i
 done
 
