505c5695dd11e3b36b066499187ee4a6925618fd
Othman Madjoudj <athmanem@gmail.com>
2020-05-16 22:43:37 +0200

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -1,8 +1,8 @@
-sudo apt install ecryptfs-utils cryptsetup vim vim-gtk3 virtualbox vagrant byobu build-essential gufw ufw \
-         gnome-tweak-tool gnome-shell-extension-caffeine clamtk clamdscan git fonts-powerline xterm putty \
-         calibre fonts-terminus xfonts-terminus sshuttle autossh i3 rofi ansible ansible-lint aria2 whois \
-         qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager virtinst wireshark nmap \
-         geoip-bin firetools firejail 
+sudo apt -y  install ecryptfs-utils cryptsetup vim vim-gtk3 virtualbox vagrant byobu build-essential gufw ufw \
+             gnome-tweak-tool gnome-shell-extension-caffeine clamtk clamdscan git fonts-powerline xterm putty \
+             calibre fonts-terminus xfonts-terminus sshuttle autossh i3 rofi ansible ansible-lint aria2 whois \
+             qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager virtinst wireshark nmap \
+             geoip-bin firetools firejail 
 
 
 sudo apt remove nfs-kernel-server rpcbind
@@ -12,6 +12,22 @@ sudo snap install  keepassxc spotify vlc
 for i in code zaproxy skype ; do
   sudo snap install --classic $i
 done
+
+
+## Cleaning up default install
+
+sudo apt -y remove nfs-kernel-server rpcbind
+
+##TODO: Maybe mask them as well
+
+# If printing is not needed
+sudo systemctl disable cups.socket cups.path cups.service
+sudo systemctl kill --signal=SIGKILL cups.service
+sudo systemctl stop cups.socket cups.path
+
+# Disable Avahi 
+sudo systemctl disable avahi-daemon.socket avahi-daemon.service
+sudo systemctl stop avahi-daemon.socket avahi-daemon.service
 
 
 
