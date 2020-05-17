f5cd4720cf2a3655d7a92d353b378033aa030976
Othman Madjoudj <athmanem@gmail.com>
2020-05-17 22:28:20 +0200

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -7,7 +7,7 @@ sudo apt -y  install ecryptfs-utils cryptsetup vim vim-gtk3 virtualbox vagrant b
              qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager virtinst wireshark nmap \
              geoip-bin firetools firejail 
 
-sudo snap install  keepassxc spotify vlc
+sudo snap install  keepassxc spotify vlc ffmpeg
 
 for i in code skype ; do
   sudo snap install --classic $i
