3f874b57eaad14dae70ba4d4aa1da8ce416a98f3
Othman Madjoudj <omadjoudj@mirantis.com>
2020-12-28 17:11:04 +0100

    Add rclone sync

--- backup.sh
+++ backup.sh
@@ -2,3 +2,5 @@
 
 
 tar pcf - $HOME | gzip | openssl enc -aes-256-cbc -pbkdf2 -e -in - -out  ~/backup/laptop-backup-partial-`date --rfc-3339=date`.tar.gz.openssl_aes-256-cbc
+rclone mkdir gdrive:laptop-backup-important-files
+rclone sync $HOME/backup gdrive:laptop-backup-important-files
