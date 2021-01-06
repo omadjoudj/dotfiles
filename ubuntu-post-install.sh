212b896c348054a7237b12fbca711aa24fec7748
Othman Madjoudj <omadjoudj@mirantis.com>
2021-01-06 23:21:31 +0100

    Sync

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -15,7 +15,7 @@ sudo snap install  keepassxc spotify vlc ffmpeg spotify #onlyoffice-desktopedito
 #  sudo snap install --classic $i
 #done
 
-EXT_LIST="golang.go ms-azuretools.vscode-docker ms-kubernetes-tools.vscode-kubernetes-tools ms-python.python ms-toolsai.jupyter ms-vscode-remote.remote-containers redhat.vscode-yaml streetsidesoftware.code-spell-checker vscodevim.vim vscoss.vscode-ansible"
+EXT_LIST="golang.go ms-python.python ms-toolsai.jupyter redhat.vscode-yaml streetsidesoftware.code-spell-checker vscodevim.vim vscoss.vscode-ansible ms-vscode.cpptools"
 
 for i in $EXT_LIST; do
       code --install-extension $i
