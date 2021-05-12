64161886c4741a55a242c532cfee436d2a0c71cd
Othman Madjoudj <athmanem@gmail.com>
2021-05-13 01:26:40 +0200

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -69,7 +69,10 @@ for i in $EXT_LIST; do
       code --install-extension $i
 done
 
+## Google chrome (some stuf does not work with Firefox)
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 ## Docker
 
