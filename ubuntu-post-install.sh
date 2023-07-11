5d8a6973553ba9f7d813062723c74cea0cb64db8
Othman Madjoudj <omadjoudj@outlook.com>
2023-07-11 17:25:12 +0200

    Sync ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -74,19 +74,19 @@ done
 
 ## Google chrome (some stuf does not work with Firefox)
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 ## Brave
-sudo apt install -y apt-transport-https curl
+#sudo apt install -y apt-transport-https curl
 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+#sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+#echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-sudo apt update
+#sudo apt update
 
-sudo apt install -y brave-browser
+#sudo apt install -y brave-browser
 
 ## Docker
 
